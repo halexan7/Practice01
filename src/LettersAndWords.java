d0297cb95cb22f2a276775c3b83db28011d40001
halexan7 <halexan7@msudenver.edu>
2022-04-18 22:05:21 -0600

    Adding LettersAndWords.java

---
+++ src/LettersAndWords.java
@@ -0,0 +1,17 @@
+// This program reverses the letters in words
+// then reverses the words in a string
+
+import java.util.*;
+
+public class LettersAndWords {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        ArrayList<Character> reverse = new ArrayList<>();
+        System.out.println("Please enter a String of characters: ");
+        String str = input.nextLine();
+        reverse.add(input.nextLine());
+        for (int i = str.length(); i < str.length(); i--) {
+            reverse.add(input.nextLine());
+        }
+    }
+}
