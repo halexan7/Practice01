db9dc469f61c00ade3a232719514078eaabc21f7
halexan7 <halexan7@msudenver.edu>
2022-04-19 21:04:37 -0600

    Finished LettersAndWords.java

--- src/LettersAndWords.java
+++ src/LettersAndWords.java
@@ -1,17 +1,25 @@
 // This program reverses the letters in words
 // then reverses the words in a string
+// Uses .split() method to recognize words in string
 
 import java.util.*;
 
 public class LettersAndWords {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        ArrayList<Character> reverse = new ArrayList<>();
+        String str;
+        ArrayList<String> reverse = new ArrayList<>();
         System.out.println("Please enter a String of characters: ");
-        String str = input.nextLine();
-        reverse.add(input.nextLine());
-        for (int i = str.length(); i < str.length(); i--) {
-            reverse.add(input.nextLine());
+        str = input.nextLine();
+        List<String> rev = Arrays.asList(str.split(" "));
+        System.out.println(rev);
+        System.out.println(rev.size());
+        for (int x = 0; x < rev.size(); x++){
+            String temp = "";
+            for (int j = rev.get(x).length(); j > 0; j--){
+                temp += rev.get(x).charAt(j-1);
+            }
+            System.out.print(temp + " ");
         }
     }
 }
